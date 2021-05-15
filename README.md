07d5bc93b705c13b3f3fd32bebe3271e9da28473
d3v3l0 <noreply@com>
2021-05-15 22:53:50 +0200

    Initial commit. Yay!

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nix flake demo
+
+Based on https://www.tweag.io/blog/2020-05-25-flakes/
+
+## Usage
+
+```bash
+TODO....
+```
+
+
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A flake for building Hello World";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+
+  };
+}
